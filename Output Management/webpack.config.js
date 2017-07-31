0b75cd43b4e39fe7410965a7c2d4a5b78065fe6e
tamro <ronyai_tamas@citromail.hu>
2017-07-31 15:55:02 +0200

    Update webpack.config.js

--- Output Management/webpack.config.js
+++ Output Management/webpack.config.js
@@ -1,20 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: {
-		app: './src/index.js',
-		print: './src/print.js'
-	},
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new HtmlWebpackPlugin({
-			title: 'Output Management'
-		})
-	],
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
